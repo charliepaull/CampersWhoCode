db07e780673ac50b40948b9ecaaad791e7a1dbf2
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2019-01-17 22:15:22 -0800

    Made route for jobs page on employerController, added 'background.css' file, altered main.handlebars, added images. All of this was to get the server to display jobs on the bootcampers.handlebars file.

--- controller/employerController.js
+++ controller/employerController.js
@@ -5,21 +5,34 @@ var router = express.Router();
 
     
 
-
+// Sends User To Homepage
+//===========================
 router.get("/", function(req, res) {
     res.render("employer");
 })
 
 
+// Shows 'jobs' Table As JSON
+//================================
 router.get("/json", function(req, res) {
     var queryString = "SELECT * FROM jobs;"
     connection.query(queryString, function(err, result) {
         if (err) throw err;
         res.json(result);
     })
-})
+});
 
+//Send Route For Job Postings
+router.get("/jobs", function(req, res) {
+    var queryString = "SELECT * FROM jobs;"
+    connection.query(queryString, function (err, results) {
+        if (err) throw err;
+        res.render("bootcampers");
+    })
+});
 
+// Post Route For Form
+//==========================
 router.post("/", function(req, res) {
     jobs.createJob([
         "job_title", "employer", "employer_picture", "job_type", "description",
